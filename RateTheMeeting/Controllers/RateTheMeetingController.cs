a701af1f8193b16f85c435541d99f6da35e9e799
Erikas Neverdauskas <erikasnever@hotmail.com>
2013-05-15 19:18:22 +0300

    test

---
+++ RateTheMeeting/Controllers/RateTheMeetingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RateTheMeeting.Models;
+
+namespace RateTheMeeting.Controllers
+{
+    public class RateTheMeetingController : Controller
+    {
+        private RateTheMeetingEntities db = new RateTheMeetingEntities();
+
+        //
+        // GET: /RateTheMeeting/
+
+        public ActionResult Index()
+        {
+            return View(db.Meetings.ToList());
+        }
+    }
+}
---
+++ RateTheMeeting/Models/AccountModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Security;
+using System.Data.SqlClient;
+
+namespace RateTheMeeting.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
